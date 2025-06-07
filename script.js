03f8372c8a11a086893bb05bc6fddf3af5a2336a
VdjuegosTGC <videojuegostgc@gmail.com>
2025-06-06 18:00:23 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-// script.js
-
-// Este script es un espacio reservado para futuras funcionalidades.
-// Por ejemplo, podrías agregar un mensaje emergente, validación de formularios o animaciones.
-
 console.log("Página JuegosTGC cargada correctamente.");
